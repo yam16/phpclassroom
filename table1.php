b532e5b20b44246f20017c82e4fc9e1ca020196e
yam16 <98868417+yam16@users.noreply.github.com>
2022-03-02 20:45:00 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>JAY-Z</td>
+        <td><img src="https://cdn.theatlantic.com/thumbor/FF5pHR85-_o0vrS5avN0EliRdmU=/607x144:2473x1194/1600x900/media/img/mt/2019/08/RTS25KHE/original.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/jay_z_daily?lang=en">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Pitbull</td>
+        <td><img src="https://pbs.twimg.com/profile_images/1177433020225032193/1loE2ws__400x400.jpg" width="45px"></td>
+        <td><a href="https://web.facebook.com/pitbull?_rdc=1&_rdr">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Roseanne Park</td>
+        <td><img src="http://pm1.narvii.com/7253/224cfc14df7df113f648d7c2268531b73237a64br1-1080-1350v2_uhq.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/roses_are_rosi?lang=en">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>ลลิษา มโนบาล</td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Blackpink_Lisa_Vogue_2021.png/220px-Blackpink_Lisa_Vogue_2021.png" width="45px"></td>
+        <td><a href="https://web.facebook.com/LisaBlackpinkThFans?_rdc=1&_rdr">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>Kim Ji-soo</td>
+        <td><img src="https://i.pinimg.com/originals/40/cc/22/40cc22afe3e8fea30811b7d943e220c0.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/globaljisoo?lang=en">Twitter</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
