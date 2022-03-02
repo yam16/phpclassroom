996e455b8731428636d6a766f19672037b3ac3d6
yam16 <98868417+yam16@users.noreply.github.com>
2022-03-02 20:25:21 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("ChocolateteehaypaiTEST.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("MiPancake.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">ลภัสรดา ท้าวพรม เลขที่ 10 ห้อง ม.4/12 เลขประจำตัว 56701</h1>
+    <h1 id="font2">ลภัสรดา ท้าวพรม เลขที่ 10 ห้อง ม.4/12 เลขประจำตัว 56701</h1>
+  </body>
+</html>
